a1fa0a86cb1babe3c61b18c87f4359834ed72969
Vick Kumar <vick.kumar44@gmail.com>
2018-09-26 15:02:32 -0400

    imported schema

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const graphQLURL = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('graphql', graphQLURL({}))
+app.use('graphql', graphQLURL({schema}))
 
 app.listen(4000, () => {
   console.log('listening to port 4000')
