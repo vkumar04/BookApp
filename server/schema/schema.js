334c98706cf0a112429b59add4c5fed4cbccc135
Vick Kumar <vick.kumar44@gmail.com>
2018-09-27 07:38:04 -0400

    added author data and query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,37 @@
 const graphQL = require('graphql')
 const _ = require('lodash')
-const {GraphQLSchema, GraphQLObjectType, GraphQLString} = graphQL
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt} = graphQL
 
 //dummy data
 const books = [
   {
     name: 'Name of the Wind',
     genre: 'Fantasy',
-    id: 1
+    id: "1"
   }, {
     name: 'The Final Empire',
     genre: 'Fantasy',
-    id: 2
+    id: "2"
   }, {
     name: 'The Long Earth',
     genre: 'Sci-Fi',
-    id: 3
+    id: "3"
+  }
+]
+
+const authors = [
+  {
+    name: 'Patrick Rothfuss',
+    age: 44,
+    id: '1'
+  }, {
+    name: 'Brandon Sanderson',
+    age: 42,
+    id: '2'
+  }, {
+    name: 'Terry Pratchett',
+    age: 66,
+    id: '3'
   }
 ]
 
@@ -23,7 +39,7 @@ const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: {
-      type: GraphQLString
+      type: GraphQLID
     },
     name: {
       type: GraphQLString
@@ -35,6 +51,21 @@ const BookType = new GraphQLObjectType({
   resolve(parent, args) {}
 })
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: {
+    name: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    },
+    id: {
+      type: GraphQLID
+    }
+  }
+})
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -42,12 +73,23 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: {
         id: {
-          type: GraphQLString
+          type: GraphQLID
         }
       },
       resolve(parent, args) {
         // get data from db
         return _.find(books, {id: args.id})
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {
+        return _.find(authors, {id: args.id})
       }
     }
   }
