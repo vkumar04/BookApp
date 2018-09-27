e237cc95107abb3ac2468cd527aaadeed16b8119
Vick Kumar <vick.kumar44@gmail.com>
2018-09-27 08:26:41 -0400

    connected to mlab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const graphQLURL = require('express-graphql')
+const mongoose = require('mongoose')
 const schema = require('./schema/schema')
 
 const app = express()
+
+//connect to mlab
+mongoose.connect('mongodb://vick:asdf1234@ds249269.mlab.com:49269/bookapp', {useNewUrlParser: true})
+mongoose
+  .connection
+  .once('open', () => {
+    console.log('connected to db')
+  })
 
 app.use('/graphql', graphQLURL({schema, graphiql: true}))
 
