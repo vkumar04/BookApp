642a4728d2397d7c387359b48c0a8e8d51a7df03
Vick Kumar <vick.kumar44@gmail.com>
2018-09-27 10:22:43 -0400

    removed dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -14,42 +14,6 @@ const {
   GraphQLNonNull
 } = graphQL
 
-//dummy data
-const books = [
-  {
-    name: 'Name of the Wind',
-    genre: 'Fantasy',
-    id: "1",
-    authorid: '1'
-  }, {
-    name: 'The Final Empire',
-    genre: 'Fantasy',
-    id: "2",
-    authorid: '2'
-  }, {
-    name: 'The Long Earth',
-    genre: 'Sci-Fi',
-    id: "3",
-    authorid: '3'
-  }
-]
-
-const authors = [
-  {
-    name: 'Patrick Rothfuss',
-    age: 44,
-    id: '1'
-  }, {
-    name: 'Brandon Sanderson',
-    age: 42,
-    id: '2'
-  }, {
-    name: 'Terry Pratchett',
-    age: 66,
-    id: '3'
-  }
-]
-
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
